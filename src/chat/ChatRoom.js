71b85ca3e1cf176cdeaa634969e81718995a392f
gering2 <geringcolton@gmail.com>
2023-11-22 22:37:51 -0800

    scroll when dummy renders

--- src/chat/ChatRoom.js
+++ src/chat/ChatRoom.js
@@ -28,12 +28,16 @@ const ChatRoom = ({ auth, firestore }) => {
             displayName
         })
         setTimeout(() => {
-            dummy.current.scrollIntoView(   { behavior: 'smooth'});
-		}, 200)
+            dummy.current.scrollIntoView({ behavior: 'smooth' });
+        }, 200)
         setMessageFormValue('');
     }
-  
-    
+
+    useEffect(() => {
+       //scroll into view once mounted
+            dummy.current.scrollIntoView({ behavior: 'smooth' });
+        
+    }, [dummy.current])
     const q = query(messagesReference, orderBy('createdAt', "desc"), limit(50))
     const [messages] = useCollectionData(q, { idField: 'id' })
     return (
