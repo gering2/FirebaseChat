3d6d6ac14dced4c589fec8dfaad219728492bd6d
gering2 <geringcolton@gmail.com>
2023-11-22 22:00:26 -0800

    fix keys for messages

--- src/chat/ChatRoom.js
+++ src/chat/ChatRoom.js
@@ -3,7 +3,7 @@ import firebase from 'firebase/compat/app';
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 import { useState, useRef, useEffect } from 'react';
 import { uuidv4 } from '../tools/uuid';
-import { doc, setDoc, collection, orderBy, query, limit, onSnapshot } from 'firebase/firestore';
+import { doc, addDoc, setDoc, collection, orderBy, query, limit, onSnapshot } from 'firebase/firestore';
 
 
 const ChatRoom = ({ auth, firestore }) => {
@@ -33,25 +33,23 @@ const ChatRoom = ({ auth, firestore }) => {
         setMessageFormValue('');
     }
   
-  
-    
-    
+useEffect(() => {
+
+})
     const q = query(messagesReference, orderBy('createdAt', "desc"), limit(50))
     const [messages] = useCollectionData(q, { idField: 'id' })
-
     return (
         <div class="flex flex-col  w-full bg-neutral-300 max-h-full mr-1 ">
             <div class=" flex flex-col justify-stretch overflow-auto mt-4">
                 {messages && messages.reverse().map((message) => {
-                    return <ChatMessage displayName={message.displayName} key={message.uid} message={message} />
+                    return <ChatMessage displayName={message.displayName} key={uuidv4()} message={message} />
                 })}
-                <span ref={dummy}></span>
+                <div ref={dummy}></div>
 
             </div>
 
             <form class="relative bottom-4 mt-5 max-h-[5%] flex justify-center w-full items-center  " onSubmit={sendMessage} >
                 <input variant="outlined" class="text-dark relative top-2 font-light bg-dark-600 outline-none focus:ring-0 pl-4 px-2 py-2 rounded-md w-[98%]" value={messageFormValue} onChange={(e) => setMessageFormValue(e.target.value)}></input>
-
             </form>
 
         </div>
